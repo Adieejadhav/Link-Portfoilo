a88df7f5875e53e9167b2c9b7bfc4b9335f0c702
Adieejadhav <adijadhav504@gmail.com>
2025-05-31 22:04:15 +0530

    Update deployment with new build files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import RegistrationPage from './pages/RegistrationPage'
-import ProfilePreview from './pages/ProfilePreview'
+import RegistrationPage from './pages/RegistrationPage';
+import ProfilePreview from './pages/ProfilePreview';
 import HomePage from './pages/HomePage';
 
 function App() {
@@ -9,18 +9,14 @@ function App() {
 
   console.log(formData);
   return (
-    <>
-      <BrowserRouter basename="/Link-Portfoilo">
-      <Router>
+    <Router basename="/Link-Portfoilo">
       <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/profilePreview" element={<ProfilePreview/>} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/profilePreview" element={<ProfilePreview />} />
         <Route path="/register" element={<RegistrationPage />} />
       </Routes>
-      </Router>
-      </BrowserRouter>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
