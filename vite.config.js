80c9848ae69d1ce011e92c963b5ac1b7d3990ce3
Adieejadhav <adijadhav504@gmail.com>
2025-06-03 00:17:18 +0530

    changed vite-config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: process.env.VITE_PUBLIC_URL,
+  base: process.env.VITE_PUBLIC_URL || '/',
   plugins: [react()],
 })
