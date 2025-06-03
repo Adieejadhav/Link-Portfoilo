eb5f9918ae28586dc0531f21cb7d3385d7ac11e3
Adieejadhav <adijadhav504@gmail.com>
2025-06-03 14:17:04 +0530

    trial -2 redeploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// vite.config.js
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-export default defineConfig({
-  base: process.env.VITE_PUBLIC_URL || '/',
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    base: env.VITE_PUBLIC_URL || '/',
+    plugins: [react()],
+  };
+});
